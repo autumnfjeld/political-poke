32086259fdbe3f53c759e14eb123de405569f3c8
Autumn Fjeld <autumn@nmutua.com>
2014-02-10 14:03:27 -0800

    Basic angular communication working

--- client/src/politicalpoke.js
+++ client/src/politicalpoke.js
@@ -3,23 +3,16 @@
  * My first experiments with an api
  */
 
-// angular.module("nytquery", ['firebase'])
-// .controller("MyController", function($scope, $firebase){
-// 	 // Firebase URL
-// 	  var candiates = new Firebase("https://scorching-fire-2722.firebaseio.com/")
-// 	  	"https://ng-newsletter.firebase.com";
-// 	  // Synchronizing the items on our $scope
-// 	  $scope.items = $firebase(new Firebase(URL + '/items'));
-// 		}
-// ]);
 
 
-angular.module("PoliticalPoke", []);
 
-anglar.module('PoliticalPoke.controllers', [])
+angular.module("PoliticalPoke", ['PoliticalPoke.controllers']);
+
+angular.module('PoliticalPoke.controllers', [])
 .controller('GetDataController', function($scope){
-	$scope.nameQuery = function({
 
-	});
-
+	$scope.nameQuery = function(){
+		console.log('nameQuery function accessed', $scope.name);
+		
+	};
 });
