2a311738bcd074a30587a3fd7fa9ae5a75145fe2
Autumn Fjeld <autumn@nmutua.com>
2014-02-10 13:27:46 -0800

    First bits of angular code

--- client/src/politicalpoke.js
+++ client/src/politicalpoke.js
@@ -3,12 +3,23 @@
  * My first experiments with an api
  */
 
-angular.module("nytquery", ['firebase'])
-.controller("MyController", function($scope, $firebase){
-	 // Firebase URL
-	  var candiates = new Firebase("https://scorching-fire-2722.firebaseio.com/")
-	  	"https://ng-newsletter.firebase.com";
-	  // Synchronizing the items on our $scope
-	  $scope.items = $firebase(new Firebase(URL + '/items'));
-		}
-]);
+// angular.module("nytquery", ['firebase'])
+// .controller("MyController", function($scope, $firebase){
+// 	 // Firebase URL
+// 	  var candiates = new Firebase("https://scorching-fire-2722.firebaseio.com/")
+// 	  	"https://ng-newsletter.firebase.com";
+// 	  // Synchronizing the items on our $scope
+// 	  $scope.items = $firebase(new Firebase(URL + '/items'));
+// 		}
+// ]);
+
+
+angular.module("PoliticalPoke", []);
+
+anglar.module('PoliticalPoke.controllers', [])
+.controller('GetDataController', function($scope){
+	$scope.nameQuery = function({
+
+	});
+
+});
