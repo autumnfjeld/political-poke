3301abab4074687a4a425a36c3e6bd9e90b17fb3
Autumn Fjeld <autumn@nmutua.com>
2014-02-11 15:31:11 -0800

    Fixed non-returning promises by returning result of next function call. Meaning: set up the chain for correct propogation.

--- client/src/politicalpoke.js
+++ client/src/politicalpoke.js
@@ -7,7 +7,30 @@ var polPoke = angular.module("PoliticalPoke", []);
 
 // Service to handle initial candidate name & id info
 polPoke.service('BasicInfoService', function($http){
+	var self = this;
 	this.candidateStore = [];
+	this.current = {};
+	this.message = '';
+
+	this.nameQuery = function(fn, ln, year){
+		var apiKeyNYTCand = "a1b62a8e2df5aa511b20dd4f1e5a8bc4:11:55466580";
+		var apikey = "&api-key=" + apiKeyNYTCand;
+		// In angular, must have callback specified directly in string
+		var url = "http://api.nytimes.com/svc/elections/us/v3/finances/" + year
+				 + "/candidates/search.json?query=" + ln + apikey + '&callback=JSON_CALLBACK';
+		
+		return $http.jsonp(url)   // use $http.jsonp !!! 
+		.then(function(obj){
+			if (obj.data.results.length !== 0){
+				return self.verifyCandidate(fn, ln, obj.data.results);
+			}else{
+				$scope.message = "Sorry, we couldn't find <b>" + fn + ln + "</b> in our database"; 
+			}
+		})
+		.catch(function(e){
+			throw e;
+		});
+	};
 
 	//callback lookup for candidate campaign finance details
 	// WIERD: variables defined above are not available with a foreach 
@@ -16,7 +39,7 @@ polPoke.service('BasicInfoService', function($http){
 		for (var i = 0; i < data.length; i ++){
 		 	var item = data[i];
 			var returnedName = item.candidate.name.toLowerCase();
-			// add all incoming data to candidateStore
+			// add all returned candidates to candidateStore
 			var obj = {};
 			obj.firstname = returnedName.split(", ")[1];
 			obj.lastname  = returnedName.split(",")[0];
@@ -25,11 +48,13 @@ polPoke.service('BasicInfoService', function($http){
 			this.candidateStore.push(obj);
 			if (returnedName === ln + ", " + fn){
 				console.log('we have a match', returnedName, obj.id);
-				this.getCandCampFin(obj.id);
+				this.current = obj;
+				return this.getCandCampFin(obj.id);
 			}
 		}
 	};
 
+	// http get request to NYTimes api for candidate campaign finance data 
 	this.getCandCampFin = function(candID){
 		var apiKeyNYTCFin = "a1b62a8e2df5aa511b20dd4f1e5a8bc4:11:55466580";
 		var apikey = "&api-key=" + apiKeyNYTCFin;
@@ -39,50 +64,65 @@ polPoke.service('BasicInfoService', function($http){
 		
 		return $http.jsonp(url)
 		.then(function(obj){
-			console.log("Got campaignFinance data", obj.data.results)
+			console.log("Got campaignFinance data", obj.data.results[0]);
+			return self.handleCandCampFinData(obj.data.results[0]);
 		})
 		.catch(function(e){
 			throw e;
-			console.log("REEEE-JEC-TION")
+			console.log("REEEE-JEC-TION");
 		});
 	};
 
+	// add relevant data to local storage/database
+	this.handleCandCampFinData = function(finData){
+		console.log('finData:',finData);
+		this.current.campaignFinance = finData;
+		//look thru existing data and add to correct candidate
+		
+		for (var i = 0; i < this.candidateStore.length; i++){
+			if (this.candidateStore[i].id === finData.id){
+				this.candidateStore[i].campaignFinance = finData;
+			}
+		}
+		return this.current;
+		console.log('Updated storage with findata', this.candidateStore);
+	};
 });
 
-
 polPoke.controller('GetDataController', function($scope, $http, BasicInfoService){
-	var apiKeyNYTCand = "a1b62a8e2df5aa511b20dd4f1e5a8bc4:11:55466580";
+	$scope.displayResults = function(){
+		console.log('current', BasicInfoService.current.campaignFinance.party);
 
-	// get candidate name from user input and query api for candidate id
+		$scope.party = BasicInfoService.current.campaignFinance.party;
+		$scope.state = BasicInfoService.current.campaignFinance.state;
+		console.log($scope.party);
+// total_contributions: 549594250.46
+// total_disbursements: 737505368.05
+// total_from_individuals: 549580640.18
+// total_from_pacs: 0
+// total_receipts: 738503770.41
+// total_refunds: 8979896.3
+	};
+
 	$scope.nameQuery = function(){
 		var queryFN = $scope.name.split(" ")[0];
 		var queryLN = $scope.name.split(" ")[1];
 		var year = "2012";
-		var apikey = "&api-key=" + apiKeyNYTCand;
-		// In angular, must have callback specified directly in string
-		var url = "http://api.nytimes.com/svc/elections/us/v3/finances/" +
-				year + "/candidates/search.json?query=" 
-				+ queryLN + apikey + '&callback=JSON_CALLBACK';
 
-		// use $http.jsonp !!!
-		return $http.jsonp(url)
-		.then(function(obj){
-			console.log('got the data', obj.data.results);
-			if (obj.data.results.length !== 0){
-				BasicInfoService.verifyCandidate(queryFN, queryLN, obj.data.results);
-			}else{
-				$scope.message = "Sorry, we couldn't find <b>" + $scope.name + "</b> in our database"; 
-			}
+		BasicInfoService.nameQuery(queryFN, queryLN, year)
+		.then(function(data){
+			//console.log('coming back to controller after data processing', data);
+			$scope.displayResults();
 		})
 		.catch(function(e){
-			throw e;
-			console.log("REEEE-JEC-TION")
-		});
+			throw(e);
+		})
 	};
 });
 
 polPoke.controller('ShowDataController', function($scope, BasicInfoService){
-	$scope.firstname = BasicInfoService.firstname;
+	//console.log('BasicInfoService.current', BasicInfoService.current);
+		$scope.firstname = BasicInfoService.firstname;
 	$scope.canCampaignMoney = [];
 	$scope.loadCanCampaignMoney = function(){
 		//$scope.candidates = 
